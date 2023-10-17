43c5f6240a5a8b16d6832c3adbc50317fff172b8
Sakumoto Takuya <tak.sakumoto@gmail.com>
2023-10-17 23:55:20 +0900

    Add the ability to check parameters

--- copy_files/main.ps1
+++ copy_files/main.ps1
@@ -1,22 +1,45 @@
 # Arguments
 param (
     [string]$jsonPath,
-    [string]$destFolder = ".\out"
+    [string]$destFolder = ".\out",
+    [string]$searchFolder,
+    [int]$depth = 1,    
+    [string]$searchString
 )
 
 # Read the config file
-$config = Get-Content -Path $jsonPath | ConvertFrom-Json
+if ($jsonPath) {
+    $config = Get-Content -Path $jsonPath | ConvertFrom-Json
+    if ($config.destFolder) {
+        $destFolder = $config.destFolder
+    }
+    if ($config.searchFolder) {
+        $searchFolder = $config.searchFolder
+    }
+    if ($config.depth) {
+        $depth = $config.depth
+    }
+    if ($config.searchString) {
+        $searchString = $config.searchString
+    }
+}
+
+# Check required variables
+$requiredVars = @($destFolder, $searchFolder, $depth, $searchString)
+foreach ($var in $requiredVars) {
+    if ([string]::IsNullOrEmpty($var)) {
+        Write-Host "Error: Required parameters are missing."
+        exit 1
+    }
+}
 
 # Make a foloder to save output files
-if ($config.destFolder) {
-    $destFolder = $config.destFolder
-}
 New-Item -Path $destFolder -ItemType Directory -Force
 $destFolder = Convert-Path $destFolder
 
 # Searche for files in the target folder based on the specified depth and search string
-$searchFolder = Convert-Path $config.searchFolder
-$files = Get-ChildItem -Path $searchFolder -Recurse -Depth $config.depth | Where-Object { $_.Name -like "*$($config.searchString)*" }
+$searchFolder = Convert-Path $searchFolder
+$files = Get-ChildItem -Path $searchFolder -Recurse -Depth $depth | Where-Object { $_.Name -like "*$($searchString)*" }
 
 # Copy the found files to the destination folder
 $fileCopied = @{}
@@ -29,3 +52,6 @@ foreach ($file in $files | Sort-Object CreationTime) {
     Copy-Item -Path $file.FullName -Destination $destFolder -Recurse
     $fileCopied[$file.Name] = $true
 }
+
+Write-Host "Done."
+exit 0
